84b4ee01ae9c6fcef3084d355b18292205b242ec
sanjaysah2006 <sanjaysha2412@gmail.com>
2025-02-01 22:12:16 +0530

    NumberGuessinngGame.cpp

--- NumberGuessinngGame.cpp
+++ NumberGuessinngGame.cpp
@@ -5,8 +5,11 @@ using namespace std;
 int main(){
     srand(time(0));
     int l,h;
-    cout << "Enter Range for guessing number (low high): ";
-    cin>>l>>h;
+    do {
+        cout << "Enter Range for guessing number (low high): ";
+        cin >> l >> h;
+        if (h < l) cout << "Enter Correct Order: low should be less than high." << endl;
+    } while (h < l);
     int RandomNumber = rand() % (h - l + 1) + l;
     cout << "Guess the number between " << l << " and " << h << ": \n";
     int GuessNumber , Count = 0 ;
@@ -14,10 +17,14 @@ int main(){
         cout << "Guess the number : ";
         cin >> GuessNumber;
         Count ++;
-        if (GuessNumber > RandomNumber)  cout << "Too High" << endl;
-        else if (GuessNumber < RandomNumber)  cout << "Too low" << endl;
-        else cout << "You guessed the correct Number in " << Count << " attemps" << endl;
-
+         if (GuessNumber > h || GuessNumber < l) {
+            cout << "Enter a number according to the range [" << l << ", " << h << "]." << endl;
+        }
+        else {
+            if (GuessNumber > RandomNumber) cout << "Too High!" << endl;
+            else if (GuessNumber < RandomNumber) cout << "Too Low!" << endl;
+            else cout << "You guessed the correct number in " << Count << " attempts!" << endl;
+        }
      }
      while(GuessNumber != RandomNumber);
 return 0;
